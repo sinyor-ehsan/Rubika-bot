dc3ababd0850df07a630efb3e0bed53401776eb6
sinyor-ehsan <xchgdgjgfuujh@gmail.com>
2025-11-13 00:21:59 -0800

    Metadata_Mode.php

--- src/Metadata/Metadata_Mode.php
+++ src/Metadata/Metadata_Mode.php
@@ -28,11 +28,6 @@ class Markdown {
         return strlen(mb_convert_encoding($text, 'UTF-16BE')) / 2;
     }
 
-    private static function utf16Len($str) {
-        return strlen(mb_convert_encoding($str, 'UTF-16BE', 'UTF-8')) / 2;
-    }
-
-
     public function toMetadata2($text){
         preg_match_all($this->MARKDOWN_RE, $text, $matches, PREG_OFFSET_CAPTURE);
 
@@ -59,77 +54,6 @@ class Markdown {
         }
 
         return $metaDataParts;
-    }
-
-    public function transcribe($src) {
-        $payloadParts = [];
-        $normalizedText = $src;
-        $byteOffset = 0;
-        $charOffset = 0;
-
-        preg_match_all(self::$pattern, $src, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
-
-        foreach ($matches as $m) {
-            $whole = $m[0][0];
-            $start = $m[0][1];
-            $end = $start + strlen($whole);
-
-            $adjFrom = self::utf16Len(substr($src, 0, $start)) - $byteOffset;
-            $adjCharFrom = $start - $charOffset;
-
-            $gname = null;
-            foreach (self::$typeMap as $key => $type) {
-                if (!empty($m[$key][0])) {
-                    $gname = $key;
-                    break;
-                }
-            }
-            if (!$gname) continue;
-
-            $inner = "";
-            $linkHref = null;
-            if ($gname === "link") {
-                $inner = $m["link_text"][0] ?? "";
-                $linkHref = $m["link_url"][0] ?? "";
-            } else {
-                $inner = $m["{$gname}_c"][0] ?? "";
-            }
-
-            if ($gname === "quote") {
-                $innerMeta = $this->transcribe($inner, "MARKDOWN");
-                $inner = $innerMeta["text"];
-                if (!empty($innerMeta["metadata"]["meta_data_parts"])) {
-                    foreach ($innerMeta["metadata"]["meta_data_parts"] as $part) {
-                        $part["from_index"] += $adjFrom;
-                        $payloadParts[] = $part;
-                    }
-                }
-            }
-
-            if ($inner === "") continue;
-
-            $contentLen = self::utf16Len($inner);
-            $part = [
-                "type" => self::$typeMap[$gname] ?? "Unknown",
-                "from_index" => $adjFrom,
-                "length" => $contentLen,
-            ];
-            if ($linkHref) {
-                $part["link_url"] = $linkHref;
-            }
-            $payloadParts[] = $part;
-
-            $normalizedText = substr($normalizedText, 0, $adjCharFrom) . $inner . substr($normalizedText, $end - $charOffset);
-            $byteOffset += self::utf16Len($whole) - $contentLen;
-            $charOffset += strlen($whole) - strlen($inner);
-        }
-
-        $result = ["text" => trim($normalizedText)];
-        if (!empty($payloadParts)) {
-            $result["meta_data_parts"] = $payloadParts;
-        }
-        
-        return $result;
     }
 
     public function toMetadata($text) {
